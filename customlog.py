d858984a7a8fd92590da769c3d0ebc7c551e3136
wangxiaolei <15835826467@163.com>
2017-08-08 11:50:25 +0800

    added file customlog.py

---
+++ customlog.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# FileName: customlog.py
+#     Date: 2017/08/08 10:00
+#   Author: 王晓磊
+#    Email: 15835826467@163.com
+# =============================================================================
+import logging
+import logging.handlers
+     
+class Logger():
+    def __init__(self, tag="log", filename="python.log"):
+        self.__logger = logging.getLogger(tag)
+          
+        # 防止重复记录日志的问题
+        if not self.__logger.handlers:
+
+            formatter = logging.Formatter('%(asctime)s %(name)s [%(levelname)s] "%(message)s"')
+            self.__logger.setLevel(logging.DEBUG)
+  
+            fh = logging.FileHandler(filename)
+            fh.setLevel(logging.DEBUG)
+            fh.setFormatter(formatter)
+            self.__logger.addHandler(fh)
+              
+    def getlogger(self):
+        return self.__logger
+  
+          
+def main():
+    print ("python logger")
+  
+      
+if __name__ == "__main__":
+    main()
